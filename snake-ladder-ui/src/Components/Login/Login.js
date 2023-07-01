f2d3d571da5cf35aecf1bf7f7e48d33efd0bf286
hansikaBuilt <hansikar@builtvisor.com>
2023-07-01 22:09:22 +0530

    intial cors

--- snake-ladder-ui/src/Components/Login/Login.js
+++ snake-ladder-ui/src/Components/Login/Login.js
@@ -1,11 +1,80 @@
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import baseURL from '../../config';
+// const Login = () => {
+//   const [userData, setUserData] = useState({
+//     emailId: '',
+//     password: ''
+//   });
+
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+//     setUserData((prevData) => ({
+//       ...prevData,
+//       [name]: value
+//     }));
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     axios.post(`${baseURL}/player/login`, userData)
+//       .then((response) => {
+//         console.log(response.data);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   };
+
+//   return (
+//     <div className="container">
+//       <h1>Login</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div className="mb-3">
+//           <label htmlFor="emailId" className="form-label">Email</label>
+//           <input
+//             type="email"
+//             className="form-control"
+//             id="emailId"
+//             name="emailId"
+//             value={userData.emailId}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <div className="mb-3">
+//           <label htmlFor="password" className="form-label">Password</label>
+//           <input
+//             type="password"
+//             className="form-control"
+//             id="password"
+//             name="password"
+//             value={userData.password}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <button type="submit" className="btn btn-primary">Login</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+
+
+
+
 import React, { useState } from 'react';
 import axios from 'axios';
 import baseURL from '../../config';
+
 const Login = () => {
   const [userData, setUserData] = useState({
     emailId: '',
     password: ''
   });
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -17,7 +86,31 @@ const Login = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post(`${baseURL}/player/login`, userData)
+
+    // Compulsory field validation
+    if (userData.emailId.trim() === '') {
+      setEmailError('Please enter your email.');
+      return;
+    }
+
+    if (userData.password.trim() === '') {
+      setPasswordError('Please enter your password.');
+      return;
+    }
+
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(userData.emailId)) {
+      setEmailError('Please enter a valid email address.');
+      return;
+    }
+
+    // Reset validation errors
+    setEmailError('');
+    setPasswordError('');
+
+    axios
+      .post(`${baseURL}/player/login`, userData)
       .then((response) => {
         console.log(response.data);
       })
@@ -31,28 +124,36 @@ const Login = () => {
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="emailId" className="form-label">Email</label>
+          <label htmlFor="emailId" className="form-label">
+            Email
+          </label>
           <input
             type="email"
-            className="form-control"
+            className={`form-control ${emailError ? 'is-invalid' : ''}`}
             id="emailId"
             name="emailId"
             value={userData.emailId}
             onChange={handleChange}
           />
+          {emailError && <div className="invalid-feedback">{emailError}</div>}
         </div>
         <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
           <input
             type="password"
-            className="form-control"
+            className={`form-control ${passwordError ? 'is-invalid' : ''}`}
             id="password"
             name="password"
             value={userData.password}
             onChange={handleChange}
           />
+          {passwordError && <div className="invalid-feedback">{passwordError}</div>}
         </div>
-        <button type="submit" className="btn btn-primary">Login</button>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
       </form>
     </div>
   );
--- snake-ladder-ui/src/Components/Signup/Signup.js
+++ snake-ladder-ui/src/Components/Signup/Signup.js
@@ -1,3 +1,83 @@
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import baseURL from '../../config';
+
+// const Signup = () => {
+//   const [userData, setUserData] = useState({
+//     name: '',
+//     emailId: '',
+//     password: ''
+//   });
+
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+//     setUserData((prevData) => ({
+//       ...prevData,
+//       [name]: value
+//     }));
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     axios.post(`${baseURL}/player/register`, userData)
+//       .then((response) => {
+//         console.log(response.data);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   };
+
+//   return (
+//     <div className="container">
+//       <h1>Signup</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div className="mb-3">
+//           <label htmlFor="name" className="form-label">Name</label>
+//           <input
+//             type="text"
+//             className="form-control"
+//             id="name"
+//             name="name"
+//             value={userData.name}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <div className="mb-3">
+//           <label htmlFor="emailId" className="form-label">Email</label>
+//           <input
+//             type="email"
+//             className="form-control"
+//             id="emailId"
+//             name="emailId"
+//             value={userData.emailId}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <div className="mb-3">
+//           <label htmlFor="password" className="form-label">Password</label>
+//           <input
+//             type="password"
+//             className="form-control"
+//             id="password"
+//             name="password"
+//             value={userData.password}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <button type="submit" className="btn btn-primary">Sign Up</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Signup;
+
+
+
+
+
+
 import React, { useState } from 'react';
 import axios from 'axios';
 import baseURL from '../../config';
@@ -8,6 +88,9 @@ const Signup = () => {
     emailId: '',
     password: ''
   });
+  const [passwordError, setPasswordError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [nameError, setNameError] = useState('');
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -19,8 +102,50 @@ const Signup = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post(`${baseURL}/player/register`, userData)
+
+    // Compulsory field validation
+    if (userData.name.trim() === '') {
+      setNameError('Please enter your name.');
+      return;
+    }
+
+    if (userData.emailId.trim() === '') {
+      setEmailError('Please enter your email.');
+      return;
+    }
+
+    if (userData.password.trim() === '') {
+      setPasswordError('Please enter your password.');
+      return;
+    }
+
+    // Password validation
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordRegex.test(userData.password)) {
+      setPasswordError(
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, one special character, and be at least 8 characters long.'
+      );
+      return;
+    }
+
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(userData.emailId)) {
+      setEmailError('Please enter a valid email address.');
+      return;
+    }
+
+    // Reset validation errors
+    setNameError('');
+    setPasswordError('');
+    setEmailError('');
+
+    axios
+      .post(`${baseURL}/player/register`, userData)
       .then((response) => {
+        if (response.status === 201) {
+          window.location.replace(`${window.location.origin}/`);
+        } 
         console.log(response.data);
       })
       .catch((error) => {
@@ -33,39 +158,50 @@ const Signup = () => {
       <h1>Signup</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="name" className="form-label">Name</label>
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
           <input
             type="text"
-            className="form-control"
+            className={`form-control ${nameError ? 'is-invalid' : ''}`}
             id="name"
             name="name"
             value={userData.name}
             onChange={handleChange}
           />
+          {nameError && <div className="invalid-feedback">{nameError}</div>}
         </div>
         <div className="mb-3">
-          <label htmlFor="emailId" className="form-label">Email</label>
+          <label htmlFor="emailId" className="form-label">
+            Email
+          </label>
           <input
             type="email"
-            className="form-control"
+            className={`form-control ${emailError ? 'is-invalid' : ''}`}
             id="emailId"
             name="emailId"
             value={userData.emailId}
             onChange={handleChange}
           />
+          {emailError && <div className="invalid-feedback">{emailError}</div>}
         </div>
         <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
           <input
             type="password"
-            className="form-control"
+            className={`form-control ${passwordError ? 'is-invalid' : ''}`}
             id="password"
             name="password"
             value={userData.password}
             onChange={handleChange}
           />
+          {passwordError && <div className="invalid-feedback">{passwordError}</div>}
         </div>
-        <button type="submit" className="btn btn-primary">Sign Up</button>
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
       </form>
     </div>
   );
